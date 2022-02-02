489e081b29cb3a438d39253b09e5e5bc98ff7ed0
alireza <alireza.mastery@gmail.com>
2022-02-02 23:39:40 +0330

    added lazy serializer utility

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from .models import User, Profile
+from utils.serializer import lazy_serializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    # user = lazy_serializer('users.serializers.UserSerializer')(read_only=True) # TODO
+
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'avatar']
