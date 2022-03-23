c0c93c997c49b0b2d525a0a9eb1d9f05b3ec4f66
alireza <alireza.mastery@gmail.com>
2022-03-23 21:27:08 +0430

    minor changes

--- persia_atlas/settings.py
+++ persia_atlas/settings.py
@@ -217,8 +217,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM':                       'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME':          timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME':  timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME':          timedelta(days=7),
+    'SLIDING_TOKEN_REFRESH_LIFETIME':  timedelta(days=14),
 }
 
 DIGIKALA_LOGIN_URL = 'https://seller.digikala.com/account/login/?_back=https://seller.digikala.com/'
