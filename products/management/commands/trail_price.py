c7be4e48997dffa6fd228f23e58a71f8dfa98781
alireza <alireza.mastery@gmail.com>
2022-03-26 13:26:19 +0430

    write errors in "trail_price robot" to a file

--- products/management/commands/trail_price.py
+++ products/management/commands/trail_price.py
@@ -1,7 +1,10 @@
+import traceback
+
 from django.core.management import BaseCommand
 
 # from products.robot.robots.trail_price_v0 import TrailingPriceRobot
 from products.robot.robots.trail_price_v1 import TrailingPriceRobot
+from utils.logging import get_tehran_datetime
 
 
 class Command(BaseCommand):
@@ -16,4 +19,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dkp = options.get('dkp')
         robot = TrailingPriceRobot(dkp=dkp)
-        robot.run()
+        try:
+            robot.run()
+        except:
+            with open('robot_errors.txt', 'a', encoding='utf-8') as log_file:
+                date = get_tehran_datetime()
+                log_file.write(f'{date:-^100}\n')
+                traceback.print_exc(file=log_file)
+                log_file.write('\n' * 2)
+                raise
