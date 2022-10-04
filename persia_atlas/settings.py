991ac08801e1ff8cdfacf70125ca35f99f254d13
alireza <alireza.mastery@gmail.com>
2022-10-04 13:24:17 +0330

    increased jwt lifetime

--- persia_atlas/settings.py
+++ persia_atlas/settings.py
@@ -203,8 +203,8 @@ REST_FRAMEWORK = {
 # *********************************************************
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME':           timedelta(days=7),
-    'REFRESH_TOKEN_LIFETIME':          timedelta(days=14),
+    'ACCESS_TOKEN_LIFETIME':           timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME':          timedelta(days=60),
     'ROTATE_REFRESH_TOKENS':           True,
     'BLACKLIST_AFTER_ROTATION':        True,
     'UPDATE_LAST_LOGIN':               False,
